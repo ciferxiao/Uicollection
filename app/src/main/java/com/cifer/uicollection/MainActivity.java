49c164ab1a014dc8ef5841f821471e1a24edc05e
xiaojinggong <1570058539@qq.com>
2018-07-09 17:23:56 +0800

    仿支付宝密码输入

--- app/src/main/java/com/cifer/uicollection/MainActivity.java
+++ app/src/main/java/com/cifer/uicollection/MainActivity.java
@@ -1,10 +1,21 @@
 package com.cifer.uicollection;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.cifer.uicollection.PasswordUi.CustomerKeyboard;
+import com.cifer.uicollection.PasswordUi.PassWordEditText;
 
 import java.util.ArrayList;
 
@@ -55,5 +66,60 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         sensorUtil.unregisterSensor();
     }
+
+    //passwordUi 调用方法
+    private void passwordshow(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.pw_layout, null);
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(layout);
+        Window window = dialog.getWindow();
+        assert window != null;
+        window.setWindowAnimations(R.style.dial_style);
+        window.setGravity(Gravity.BOTTOM);
+        int width = WindowManager.LayoutParams.MATCH_PARENT;
+        int height =WindowManager.LayoutParams.WRAP_CONTENT;
+        WindowManager.LayoutParams lp = window.getAttributes(); // 获取对话框当前的参数值
+        lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(lp);
+        dialog.show();
+        CustomerKeyboard customerKeyboard = (CustomerKeyboard) layout.findViewById(R.id.keyboard_View);
+        final PassWordEditText pw_EditText = (PassWordEditText)layout. findViewById(R.id.pw_EditText);
+        customerKeyboard.setOnCustomerKeyboardClickListener(new CustomerKeyboard.CustomerKeyboardClickListener() {
+            @Override
+            public void click(String pw) {
+                pw_EditText.addPW(pw);
+            }
+
+            @Override
+            public void remove() {
+                pw_EditText.removePW();
+            }
+
+            @Override
+            public void dismiss() {
+                dialog.dismiss();
+            }
+        });
+
+        pw_EditText.setOnPWCommitListener(new PassWordEditText.PWCommitListener() {
+            @Override
+            public void commit_PW(String pw) {
+                if(pw.equals("1234")){
+                    Intent intent = new Intent();
+                    startActivity(intent);
+                    dialog.dismiss();
+                }else if(pw.length()>3 && !pw.equals("1234")){
+                    pw_EditText.clear();
+                }
+            }
+        });
+
+    }
+
+
+
 
 }
